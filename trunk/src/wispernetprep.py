89427dbf676a1e4228635ad39e322eb036586004
sersom84@gmail.com <sersom84@gmail.com@94dbcb3d-8299-e3b3-4035-03227f8d3779>
2014-04-13 06:58:18 +0000

    Использовал Unidecode для транслита. Должен обеспечить универсальность для большинства non-ASCII языков. Брать здесь:https://pypi.python.org/pypi/Unidecode/

--- trunk/src/wispernetprep.py
+++ trunk/src/wispernetprep.py
@@ -8,6 +8,7 @@ import glob
 from subprocess import Popen, PIPE, STDOUT
 import argparse
 import unicodefix
+from unidecode import unidecode
 
 def processFile(infile, seqnumber, title, asin):
     infilename = os.path.splitext(infile)[0]
@@ -18,7 +19,8 @@ def processFile(infile, seqnumber, title, asin):
     os.mkdir(outputdir)
     shutil.copy(infile, os.path.join(inputdir, infilename+u".mobi"))
     for file in glob.glob(os.path.join(u"images.$$$", infilename+u'.cover*')):
-        imgname = u"thumbnail_" + infilename + u"_EBOK_portrait.jpg"
+#        imgname = u"thumbnail_" + translit(infilename) + u"_EBOK_portrait.jpg"
+        imgname = u"thumbnail_" + unidecode(infilename) + u"_EBOK_portrait.jpg"
         shutil.copy(file, imgname)
         preparecover.resize(imgname)    
 
@@ -26,7 +28,10 @@ def processFile(infile, seqnumber, title, asin):
     cmd = u'java -cp "' + os.path.join(scriptdir, u"MobiMetaEditorV0.16.jar") +u'" cli.WhisperPrep "%s" "%s"' % (inputdir, outputdir)
     print u"Running", cmd
     process = Popen(cmd, stdin=PIPE, stdout=sys.stdout, stderr=STDOUT)
-    process.stdin.write(infilename.encode("utf-8") if asin is None else asin)
+#    process.stdin.write(infilename.encode("utf-8") if asin is None else asin)
+#    process.stdin.write(infilename.encode("cp1251") if asin is None else asin)
+#     process.stdin.write(translit(infilename) if asin is None else asin)
+    process.stdin.write(unidecode(infilename) if asin is None else asin)
     process.stdin.write("\n")
     process.stdin.close()
     process.wait()
@@ -42,13 +47,14 @@ def processFile(infile, seqnumber, title, asin):
         try:
             print u'Title: "%s"' % title
         except:
-            tit = translit(title)
-            print u'Title: "%s"' % tit
+            # tit = translit(title)
+            print u'Title: "%s"' % unidecode(title)
+        pass
     seqnumber = get_seqnumber(infilename, seqnumber)
     print u'Seq number: "%s"' % seqnumber
     if title is not None or seqnumber is not None:
-        preparecover.draw(u"thumbnail_" + infilename + u"_EBOK_portrait.jpg", title, seqnumber)
-    
+        preparecover.draw(u"thumbnail_" + unidecode(infilename) + u"_EBOK_portrait.jpg", title, seqnumber)
+
     return 0
 
 def get_booktitle(infile, title):
@@ -71,47 +77,6 @@ def get_seqnumber(infilename, seqnumber):
     #result = result[0:2].zfill(2)
     result = result[0:2]
     return result
-
-def translit(locallangstring):
-    conversion = {
-        u'\u0410' : 'A',    u'\u0430' : 'a',
-        u'\u0411' : 'B',    u'\u0431' : 'b',
-        u'\u0412' : 'V',    u'\u0432' : 'v',
-        u'\u0413' : 'G',    u'\u0433' : 'g',
-        u'\u0414' : 'D',    u'\u0434' : 'd',
-        u'\u0415' : 'E',    u'\u0435' : 'e',
-        u'\u0401' : 'Yo',   u'\u0451' : 'yo',
-        u'\u0416' : 'Zh',   u'\u0436' : 'zh',
-        u'\u0417' : 'Z',    u'\u0437' : 'z',
-        u'\u0418' : 'I',    u'\u0438' : 'i',
-        u'\u0419' : 'Y',    u'\u0439' : 'y',
-        u'\u041a' : 'K',    u'\u043a' : 'k',
-        u'\u041b' : 'L',    u'\u043b' : 'l',
-        u'\u041c' : 'M',    u'\u043c' : 'm',
-        u'\u041d' : 'N',    u'\u043d' : 'n',
-        u'\u041e' : 'O',    u'\u043e' : 'o',
-        u'\u041f' : 'P',    u'\u043f' : 'p',
-        u'\u0420' : 'R',    u'\u0440' : 'r',
-        u'\u0421' : 'S',    u'\u0441' : 's',
-        u'\u0422' : 'T',    u'\u0442' : 't',
-        u'\u0423' : 'U',    u'\u0443' : 'u',
-        u'\u0424' : 'F',    u'\u0444' : 'f',
-        u'\u0425' : 'H',    u'\u0445' : 'h',
-        u'\u0426' : 'Ts',   u'\u0446' : 'ts',
-        u'\u0427' : 'Ch',   u'\u0447' : 'ch',
-        u'\u0428' : 'Sh',   u'\u0448' : 'sh',
-        u'\u0429' : 'Sch',  u'\u0449' : 'sch',
-        u'\u042a' : '"',    u'\u044a' : '"',
-        u'\u042b' : 'Y',    u'\u044b' : 'y',
-        u'\u042c' : '\'',   u'\u044c' : '\'',
-        u'\u042d' : 'E',    u'\u044d' : 'e',
-        u'\u042e' : 'Yu',   u'\u044e' : 'yu',
-        u'\u042f' : 'Ya',   u'\u044f' : 'ya',
-    }
-    translitstring = []
-    for c in locallangstring:
-        translitstring.append(conversion.setdefault(c, c))
-    return ''.join(translitstring)
 
 
 def main(argv=sys.argv):
