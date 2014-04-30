6146a5c9df8d25f050aab30f277e28bd8b0d16c8
victor.zeltser@gmail.com <victor.zeltser@gmail.com@94dbcb3d-8299-e3b3-4035-03227f8d3779>
2014-04-30 18:25:25 +0000

    Unicode fix

--- branches/Embedded cover/swap_cover.py
+++ branches/Embedded cover/swap_cover.py
@@ -1,13 +1,17 @@
 import os 
 import sys
+import unicodefix
 from lib.mobi_split2 import mobi_split
-def modify_mobi(infile,outfile):
+
+def modify_mobi(infile, outfile):
     splitter = mobi_split(infile)
     open(os.path.splitext(outfile)[0] + '.azw3', 'wb').write(splitter.getResult8())
 
 def main(argv=sys.argv):
     # infile, outfile, ASIN
-    return modify_mobi(argv[1], argv[2])
+    input_file = unicode(argv[1], sys.stdin.encoding)
+    output_file = unicode(argv[2], sys.stdin.encoding)
+    return modify_mobi(input_file, output_file)
 
 if __name__ == "__main__":
     sys.exit(main())
